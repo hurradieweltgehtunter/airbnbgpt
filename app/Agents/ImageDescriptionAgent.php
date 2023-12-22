1d493c71de9b1e198c3d849ef60b63da6de61ae7
hurradieweltgehtunter <hi@florianlenz.com>
2023-12-22 09:41:05 +0100

    remove event streaming

--- app/Agents/ImageDescriptionAgent.php
+++ app/Agents/ImageDescriptionAgent.php
@@ -46,88 +46,78 @@ class ImageDescriptionAgent extends Agent
     public function run($data = null) {
         $response = [];
 
-        return new StreamedResponse(function () {
-            // Load all images from the housing
-            $housing = $this->agentable;
-            $rooms = $housing->rooms;
-            $originalInitialMessage = $this->conversation->getFirstMessage()->content;
+        // Load all images from the housing
+        $housing = $this->agentable;
+        $rooms = $housing->rooms;
+        $originalInitialMessage = $this->conversation->getFirstMessage()->content;
 
-            // Go through all available rooms
-            foreach($rooms as $room)
-            {
-                $images = $room->images;
+        // Go through all available rooms
+        foreach($rooms as $room)
+        {
+            $images = $room->images;
 
-                if(count($images) == 0) {
-                    continue;
-                }
-
-                // For each image we want to create a image description text
-                foreach($images as $image) {
-                    // Empty the conversation
-                    $this->conversation->clearMessages();
-
-                    // replace the label placeholder in the message
-                    $messageContent = str_replace('{{label}}', $room->name, $originalInitialMessage);
-
-                    // replace the writingStyle placeholder, insert the writing style
-                    $messageContent = str_replace('{{writingStyle}}', $this->writingStyle()->description, $messageContent);
-
-                    // Create the message content
-                    $content = [
-                        [
-                            "type" => "text",
-                            "text" => $messageContent
-                        ],
-                        [
-                            "type" => "image_url",
-                            "image_url" => $image->path
-                        ],
-                    ];
-
-                    // erstelle die message
-                    $message = new Message();
-                    $message->role = 'user';
-                    $message->content = $content;
-                    $message->senderId = $housing->user->id;
-                    $message->housing_id = $housing->id;
-
-                    $this->addMessage($message);
-
-                    Log::debug('ImageAnalyzer sends message to AI. Conversation:');
-                    foreach($this->conversation->getAllMessages() as $message) {
-                        Log::debug($message->role . ': ' . print_r($message->content, true));
-                    }
-
-                    [$returnMessage, $agentUsage] = parent::run();
-
-                    // get the room to $housing with label $label
-                    $image->description = parent::fixUmlauts($returnMessage->content);
-                    $image->save();
-
-                    $agentUsage->setEntity($image)
-                        ->save();
-                }
-
-                // Room is done, lets flush the data to the frontend
-                $room->load('images');
-
-                echo "data: " . json_encode(['room' => $room]) . "\n\n";
-                ob_flush();
-                flush();
+            if(count($images) == 0) {
+                continue;
             }
 
-            // Now that we have analyzed the images, delete the agent
-            // Agent::where('id', $this->id)->delete();
+            // For each image we want to create a image description text
+            foreach($images as $image) {
+                // Empty the conversation
+                $this->conversation->clearMessages();
 
-            // return ['type' => 'redirect', 'url' => route('housings.showQuestionnaire', $this->agentable->id, false)];
-            $this->has_finished = true;
-            $this->save();
-            return true;
-        }, 200, [
-            'Content-Type' => 'text/event-stream',
-            'Cache-Control' => 'no-cache',
-            'X-Accel-Buffering' => 'no', // Wichtig für Nginx, um Buffering zu deaktivieren
-        ]);
+                // replace the label placeholder in the message
+                $messageContent = str_replace('{{label}}', $room->name, $originalInitialMessage);
+
+                // replace the writingStyle placeholder, insert the writing style
+                $messageContent = str_replace('{{writingStyle}}', $this->writingStyle()->description, $messageContent);
+
+                // Create the message content
+                $content = [
+                    [
+                        "type" => "text",
+                        "text" => $messageContent
+                    ],
+                    [
+                        "type" => "image_url",
+                        "image_url" => $image->path
+                    ],
+                ];
+
+                // erstelle die message
+                $message = new Message();
+                $message->role = 'user';
+                $message->content = $content;
+                $message->senderId = $housing->user->id;
+                $message->housing_id = $housing->id;
+
+                $this->addMessage($message);
+
+                foreach($this->conversation->getAllMessages() as $message) {
+                    Log::debug($message->role . ': ' . print_r($message->content, true));
+                }
+
+                [$returnMessage, $agentUsage] = parent::run();
+
+                // get the room to $housing with label $label
+                $image->description = parent::fixUmlauts($returnMessage->content);
+                $image->save();
+
+                $agentUsage->setEntity($image)
+                    ->save();
+            }
+
+            // Room is done, lets flush the data to the frontend
+            $room->load('images');
+
+        }
+
+        // Now that we have analyzed the images, delete the agent
+        // Agent::where('id', $this->id)->delete();
+
+        // return ['type' => 'redirect', 'url' => route('housings.showQuestionnaire', $this->agentable->id, false)];
+        $this->has_finished = true;
+        $this->save();
+        return true;
     }
 
     public function finished() {
